6559afd8313359065cfb5daebc9a0c1a02058f97
EFranco <eddfranco87@gmail.com>
2024-10-29 00:07:37 -0600

    updates to can.c

--- src/can.c
+++ src/can.c
@@ -33,9 +33,9 @@ void canbus_setup(void)
     can2040_callback_config(&cbus, can2040_cb); // Register Callback, links cbus to the can2040_cb callback for handling received messages.
 
     // Enable irqs
-    irq_set_exclusive_handler(PIO0_IRQ_0, PIOx_IRQHandler);
-    irq_set_priority(PIO0_IRQ_0, PICO_DEFAULT_IRQ_PRIORITY - 1);
-    irq_set_enabled(PIO0_IRQ_0, 1);
+    irq_set_exclusive_handler(PIO0_IRQ_0, PIOx_IRQHandler); //  Assigns the interrupt handler PIOx_IRQHandler to PIO0_IRQ_0 (the interrupt for PIO0)
+    irq_set_priority(PIO0_IRQ_0, PICO_DEFAULT_IRQ_PRIORITY - 1); // Sets the priority of PIO0_IRQ_0 just above the default, so it can respond quickly without overriding critical tasks
+    irq_set_enabled(PIO0_IRQ_0, 1); // Enables PIO0_IRQ_0, allowing interrupts from this source
 
     // Start canbus
     can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
